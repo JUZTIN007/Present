d91913608190332d6e67919e81aeec908f83ee6a
JUZTIN007 <sridhar.km@leapfinance.com>
2026-04-20 18:20:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
 function start(){
     window.location.href = "story.html";
 }
-
-/* TEXT FLOW */
-let texts = [
-`There was a time…
-
-we stopped talking.
-
-No reason.
-Just silence.`,
-
-`But somehow…
-
-we came back.
-
-Not the same…
-
-better.`
-];
-
-let index = 0;
-
-function next(){
-    if(index < texts.length){
-        document.getElementById("text").innerText = texts[index];
-        index++;
-    } else {
-        window.location.href = "final.html";
-    }
-}
-
-/* AUTO START MUSIC */
-document.addEventListener("click",()=>{
-    let m = document.getElementById("music");
-    if(m) m.play();
-},{once:true});
